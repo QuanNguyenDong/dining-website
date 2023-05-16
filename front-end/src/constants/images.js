8d273d74a1729c7e4de251a874e42bd19b535250
QuanNguyenDong <nguyendongquan031102@gmail.com>
2023-05-16 22:15:03 +1000

    trying to fix bug

--- front-end/src/constants/images.js
+++ front-end/src/constants/images.js
@@ -1,14 +1,14 @@
-import noodleLogo from '../assets/Noodles-logo.png';
-import wineImage from '../assets/images/Dinner.jpg'
-import pndBanner from '../assets/images/PND-banner.jpg';
-import eggNoodleCC from '../assets/images/Egg-Noodle-CC.jpg';
+// import noodleLogo from '../assets/Noodles-logo.png';
+// import wineImage from '../assets/images/Dinner.jpg'
+// import pndBanner from '../assets/images/PND-banner.jpg';
+// import eggNoodleCC from '../assets/images/Egg-Noodle-CC.jpg';
 
-const images = {
-  // rareBeefPho,
-  noodleLogo,
-  wineImage,
-  pndBanner,
-  eggNoodleCC
-}
+// const images = {
+//   // rareBeefPho,
+//   noodleLogo,
+//   wineImage,
+//   pndBanner,
+//   eggNoodleCC
+// }
 
-export default images;
+// export default images;
